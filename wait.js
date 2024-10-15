b9c9354b821f049a2f0eaf4b6f25bf4a80cdd656
Janagar Ramaprabha <92803259+Janagar-Adappt@users.noreply.github.com>
2024-10-15 16:06:08 +0530

    Update wait.js

--- wait.js
+++ wait.js
@@ -1,18 +1,13 @@
-try {
-    await driver.waitUntil(
-      async function () {
-        try {
-          const homeIconDisplayed = await FooterHome.homeIcon.isDisplayed();
-          const isHeaderDisplayed =
-            await HomeScreen.contentListItem.isDisplayed();
-          return homeIconDisplayed && isHeaderDisplayed;
-        } catch (error) {
-          console.error(`An error occurred: ${error}`);
-          return false;
-        }
-      },
-      { timeout: 15000, timeoutMsg: "Element did not become visible" }
-    );
-  } catch (error) {
-    console.error(`An error occurred while waiting: ${error}`);
-  }
+ await driver.waitUntil(
+    async () => {
+      return await FooterHome.homeIcon.isDisplayed();
+    },
+    {
+      timeout: 5000,
+      timeoutMsg: "The home screen is not displayed within the timeout period",
+    }
+  );
+
+  await expect(FooterHome.homeIcon).toBeDisplayed();
+  await FooterHome.homeIcon.click();
+  await driver.pause(2000);
